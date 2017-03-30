d3eb5d8480e3674ebe4405c0dc2c113897f470b9
petrixPC <petrix18@gmail.com>
2017-03-30 20:15:31 +0200

    - Basic structure of the Level scene and started defining class architecture

---
+++ Lightbot-clone-Unity/Assets/Scripts/BotOperation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotOperation
+{
+	public BotOperation()
+	{
+
+	}
+
+	public void RunOperation ()
+	{
+
+	}
+
+	public void ValidateOperation()
+	{
+
+	}
+}
